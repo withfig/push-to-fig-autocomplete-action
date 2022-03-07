0fc3aba1edb85ee30a89c9c423d7303ee045e56b
Federico Ciardi <fed.ciardi@gmail.com>
2022-03-07 21:26:28 +0100

    refactor: move methods to separate file
    feat: implement integration handling

--- src/main.ts
+++ src/main.ts
@@ -1,142 +1,14 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { format } from './prettier'
-import { lintString } from './eslint'
+import {
+  checkOrCreateAutocompleteFork,
+  createAutocompleteRepoPR,
+  createCommitOnNewRepoBranch,
+  getAutocompleteRepoSpec,
+  getFormattedSpecContent
+} from './utils'
+import mergeSpecs from '@withfig/autocomplete-tools/build/merge'
 import { randomUUID } from 'crypto'
-import { Repo, Octokit, File } from './types'
-import { AUTOCOMPLETE_DEFAULT_BRANCH, AUTOCOMPLETE_REPO } from './constants'
-
-async function rebaseOnCurrentMaster(octokit: Octokit, fork: Repo) {
-  const upstreamMaster = await octokit.rest.git.getRef({
-    ...AUTOCOMPLETE_REPO,
-    ref: `heads/${AUTOCOMPLETE_DEFAULT_BRANCH}`
-  })
-  const newSha = upstreamMaster.data.object.sha
-
-  await octokit.rest.git.updateRef({
-    ...fork,
-    ref: `heads/${AUTOCOMPLETE_DEFAULT_BRANCH}`,
-    sha: newSha
-  })
-}
-
-async function checkOrCreateAutocompleteFork(octokit: Octokit): Promise<Repo> {
-  const user = await octokit.rest.users.getAuthenticated()
-  const autocompleteForks = await octokit.rest.repos.listForks(AUTOCOMPLETE_REPO)
-
-  for (let i = 0; i < autocompleteForks.data.length; i++) {
-    const fork = autocompleteForks.data[i]
-    if (fork.owner.login === user.data.login) {
-      const forkData = { owner: fork.owner.login, repo: fork.name }
-      await rebaseOnCurrentMaster(octokit, forkData)
-      return forkData
-    }
-  }
-
-  // TODO: race until the repo is created
-  await octokit.rest.repos.createFork(AUTOCOMPLETE_REPO)
-
-  return { owner: user.data.login, repo: 'autocomplete' }
-}
-
-async function getDefaultBranch(
-  octokit: Octokit,
-  repo: typeof github.context.repo
-) {
-  return (await octokit.rest.repos.get(repo)).data.default_branch
-}
-
-async function createCommitOnNewBranch(
-  octokit: Octokit,
-  fork: Repo,
-  branchName: string,
-  changedFile: File
-) {
-  // create new branch on top of the upstream master
-  const masterRef = await octokit.rest.git.getRef({
-    ...fork,
-    ref: `heads/${AUTOCOMPLETE_DEFAULT_BRANCH}`
-  })
-  const newBranchRef = await octokit.rest.git.createRef({
-    ...fork,
-    ref: `refs/heads/${branchName}`,
-    sha: masterRef.data.object.sha
-  })
-
-  // create new blob, new tree, commit everything and update PR branch
-  const newBlob = await octokit.rest.git.createBlob({
-    ...fork,
-    content: changedFile.content,
-    encoding: 'utf-8'
-  })
-  const newTree = await octokit.rest.git.createTree({
-    ...fork,
-    tree: [
-      {
-        path: changedFile.path,
-        sha: newBlob.data.sha,
-        mode: '100644',
-        type: 'blob'
-      }
-    ],
-    base_tree: newBranchRef.data.object.sha
-  })
-
-  const newCommit = await octokit.rest.git.createCommit({
-    ...fork,
-    message: 'feat: update spec',
-    tree: newTree.data.sha,
-    parents: [newBranchRef.data.object.sha]
-  })
-
-  octokit.rest.git.updateRef({
-    ...fork,
-    ref: `heads/${branchName}`,
-    sha: newCommit.data.sha
-  })
-}
-
-async function createAutocompletePR(
-  octokit: Octokit,
-  specName: string,
-  forkOwner: string,
-  branchName: string
-) {
-  const defaultBranch = await getDefaultBranch(octokit, AUTOCOMPLETE_REPO)
-  // create a new branch in the fork and create
-  await octokit.rest.pulls.create({
-    ...AUTOCOMPLETE_REPO,
-    title: `feat(${specName}): update spec`,
-    head: `${forkOwner}:${branchName}`,
-    base: defaultBranch
-  })
-}
-
-async function getFormattedSpecContent(
-  octokit: Octokit,
-  specPath: string,
-  specName: string
-): Promise<string> {
-  const specFile = await octokit.rest.repos.getContent({
-    path: specPath,
-    ...github.context.repo
-  })
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  function isFile(file: any): asserts file is { content: string } {
-    if (Array.isArray(specFile.data) || specFile.data.type !== 'file')
-      throw new Error(
-        `spec-path: ${specPath} does not correspond to a valid file`
-      )
-  }
-
-  isFile(specFile.data)
-
-  const decodedFile = Buffer.from(specFile.data.content, 'base64').toString()
-
-  const lintedString = lintString(decodedFile, `${specName}.ts`)
-  return format(lintedString)
-}
 
 async function run() {
   try {
@@ -145,28 +17,44 @@ async function run() {
       required: true
     })
     const specPath = core.getInput('spec-path', { required: true })
+    const integration = core.getInput('integration')
 
     const octokit = github.getOctokit(token)
 
     // get generated spec, run eslint and prettier on top of it and report eventual errors
-    const generatedSpecContent = await getFormattedSpecContent(
+    let newSpecContent = await getFormattedSpecContent(
       octokit,
       specPath,
       autocompleteSpecName
     )
+
+    // check if spec already exist in autocomplete repo, if it does => run merge tool and merge it
+    if (integration) {
+      const autocompleteSpec = await getAutocompleteRepoSpec(octokit, specPath)
+      if (autocompleteSpec) {
+        newSpecContent = mergeSpecs(autocompleteSpec, newSpecContent, {
+          preset: integration
+        })
+      }
+    }
 
     // create autocomplete fork
     const autocompleteFork = await checkOrCreateAutocompleteFork(octokit)
 
     // commit the file to a new branch on the autocompletefork
     const newBranchName = `auto-update/${autocompleteSpecName}/${randomUUID()}`
-    await createCommitOnNewBranch(octokit, autocompleteFork, newBranchName, {
-      path: `src/${autocompleteSpecName}.ts`,
-      content: generatedSpecContent
-    })
+    await createCommitOnNewRepoBranch(
+      octokit,
+      autocompleteFork,
+      newBranchName,
+      {
+        path: `src/${autocompleteSpecName}.ts`,
+        content: newSpecContent
+      }
+    )
 
     // create a PR from the branch with changes
-    await createAutocompletePR(
+    await createAutocompleteRepoPR(
       octokit,
       autocompleteSpecName,
       autocompleteFork.owner,
--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,18 @@ import type { getOctokit } from '@actions/github'
 
 export type Octokit = ReturnType<typeof getOctokit>
 
+export interface OctokitError {
+  name: string
+  status: number
+  documentation_url: string
+  errors?: {
+    resource: string
+    code: string
+    field: string
+    message?: string
+  }[]
+}
+
 export type Repo = { owner: string; repo: string }
 
 export interface File {
