0ba824354f81c537c5079e992a7858598622dca3
Federico Ciardi <fed.ciardi@gmail.com>
2022-03-11 01:14:43 +0100

    chore: more useful output messages

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { File, Octokit } from './types'
 import { format } from './prettier'
@@ -14,16 +15,23 @@ export async function getFormattedSpecContent(
   octokit: Octokit,
   specPath: string
 ): Promise<string> {
+  core.info('Started retrieving the new completion spec from the repo...')
   const specFile = await octokit.rest.repos.getContent({
     ...github.context.repo,
     path: specPath
   })
+  core.info('Finished retrieving new completion spec from the repo')
 
   if (isFile(specFile.data)) {
+    core.info('Started decoding the new spec...')
     const decodedFile = Buffer.from(specFile.data.content, 'base64').toString()
+    core.info('Finished decoding the new spec')
 
+    core.info('Started linting and formatting the new spec...')
     const lintedString = await lintString(decodedFile, specPath)
-    return format(lintedString)
+    const formatted = format(lintedString)
+    core.info('Finished linting and formatting the new spec')
+    return formatted
   }
   throw new Error(`spec-path: ${specPath} does not correspond to a valid file`)
 }
