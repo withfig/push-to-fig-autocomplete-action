d89ccd9ff070393f7a55b4b8fc67f7b89f96f045
figbot <82115609+withfig-bot@users.noreply.github.com>
2022-03-31 12:48:29 -0700

    feat: update spec

--- src/__integration-tests__/diff-based-versioning/cli/1.0.0.ts
+++ src/__integration-tests__/diff-based-versioning/cli/1.0.0.ts
@@ -4,7 +4,7 @@ const completion: Fig.Subcommand = {
   subcommands: [
     {
       name: 'remove',
-      args: { name: "name" },
+      args: { name: 'name' },
     },
   ],
 }
@@ -12,6 +12,16 @@ const completion: Fig.Subcommand = {
 const versions: Fig.VersionDiffMap = {}
 
 versions['1.0.0'] = {}
+
+versions['1.1.0'] = {
+  description: 'Updated description',
+  subcommands: [
+    {
+      name: 'update',
+      description: 'New command description',
+    },
+  ],
+}
 
 export { versions }
 export default completion
