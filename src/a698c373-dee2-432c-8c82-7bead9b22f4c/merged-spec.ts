d7b3bbd74fab0e20f66fe69b253629e118c58744
figbot <82115609+withfig-bot@users.noreply.github.com>
2022-03-31 05:55:03 -0700

    feat: update spec

---
+++ src/a698c373-dee2-432c-8c82-7bead9b22f4c/merged-spec.ts
@@ -0,0 +1,15 @@
+const completionSpec: Fig.completionSpec = {
+  name: 'fig',
+  description: 'Updated description',
+  subcommands: [
+    {
+      name: 'update',
+      description: 'Updated description',
+      icon: 'https://path.com'
+    },
+    {
+      name: 'remove'
+    }
+  ]
+}
+export default completionSpec
