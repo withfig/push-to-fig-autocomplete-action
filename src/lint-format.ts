efacf0ac9e828a14150bda25a1a99cb79451555d
Federico Ciardi <fed.ciardi@gmail.com>
2022-04-26 18:41:28 +0200

    test

--- src/lint-format.ts
+++ src/lint-format.ts
@@ -9,7 +9,7 @@ export async function runEslintOnPath(path: string) {
   })
   await execAsync('npm i @fig/eslint-config-autocomplete@latest eslint@latest')
   await execAsync(
-    `npx eslint@latest --no-ignore --debug --config .tmp-eslintrc --fix ${path}`
+    `npx eslint@latest --debug --config .tmp-eslintrc --fix ${path}`
   )
   core.info('Finished running eslint on spec file')
 }
