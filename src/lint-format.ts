5e504202f748c4c54ea5d254059377ebc6294386
Federico Ciardi <fed.ciardi@gmail.com>
2022-04-26 21:56:12 +0200

    fix: ignore `.eslintrc.*` config files when running eslint (#81)

--- src/lint-format.ts
+++ src/lint-format.ts
@@ -2,19 +2,23 @@ import * as core from '@actions/core'
 import { execAsync } from './utils'
 import { writeFile } from 'fs/promises'
 
-export async function runEslintOnPath(path: string) {
+async function runEslintOnPath(path: string) {
   core.info(`Started running eslint on spec file: ${path}`)
-  await writeFile('.tmp-eslintrc', '{"extends":"@fig/autocomplete"}', {
-    encoding: 'utf8'
-  })
-  await execAsync('npm i @fig/eslint-config-autocomplete')
+  await writeFile(
+    '.tmp-eslintrc',
+    '{"root": true,"extends":"@fig/autocomplete"}',
+    {
+      encoding: 'utf8'
+    }
+  )
+  await execAsync('npm i @fig/eslint-config-autocomplete@latest eslint@8')
   await execAsync(
-    `npx eslint@8 --config .tmp-eslintrc --no-ignore --fix ${path}`
+    `npx eslint@8 --no-ignore --no-eslintrc --config .tmp-eslintrc --fix ${path}`
   )
   core.info('Finished running eslint on spec file')
 }
 
-export async function runPrettierOnPath(path: string) {
+async function runPrettierOnPath(path: string) {
   core.info(`Started running prettier on spec file: ${path}`)
   await execAsync(
     `npx prettier@2 ${path} --no-config --write --parser typescript`
